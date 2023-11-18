36a1f5d2aa2c9f31bca0fcc602dce8e6b89a0054
AP-51 <ai21btech11002@iith.ac.in>
2023-11-18 16:49:55 +0530

    updated flask api

--- Flask-API/server.py
+++ Flask-API/server.py
@@ -10,26 +10,29 @@ def process_input():
     intensity, bssid = input_data['intensity'], input_data['bssid']
 
     print("Input from user", intensity, bssid)
+    print(type(bssid))
 
     # Connect to the existing SQL database
     conn = sqlite3.connect('data.db')
     cursor = conn.cursor()
 
     # Query the database to find the corresponding grid based on intensity and BSSID
+    cursor.execute(f"SELECT classroom FROM class_bssid WHERE bssid='{bssid}'")
+    room=cursor.fetchone()
     cursor.execute("SELECT grid_id, Classroom FROM intensity_bssid WHERE intensity=? AND bssid=?", (intensity, bssid))
     result = cursor.fetchone()
 
-    if result:
-        grid_id, classroom = result[0], result[1]
+    if True:
+        #grid_id, classroom = result[0], result[1]
 
         # Query the database to retrieve the classroom details based on the grid_id
-        cursor.execute("SELECT room, floor, block, location FROM classrooms WHERE id=?", (grid_id,))
-        classroom_info = cursor.fetchone()
+        #cursor.execute("SELECT room, floor, block, location FROM classrooms WHERE id=?", (grid_id,))
+        #classroom_info = cursor.fetchone()
 
         # Close the database connection
         conn.close()
-
-        response_data = {'grid_name': grid_id, 'classroom': classroom_info[0], 'floor': classroom_info[1], 'block': classroom_info[2]}
+        response_data ={'classroom':room,'bssid':bssid}
+        #response_data = {'grid_name': grid_id, 'classroom': room, 'floor': classroom_info[1], 'block': classroom_info[2]}
         print(response_data)
         return jsonify(response_data)
     else:
